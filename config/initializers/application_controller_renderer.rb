ff8aaf7fe5426ddf30f83d61df55a7d955a94278
YoshikiHayashi <yoshikixyz57@gmail.com>
2020-12-23 10:23:39 +0000

    commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '41331a72ac71fb0d001bbf19188dd2bcc11cd12a18fa2b409136d93bdafde715a6e3def47fac4f15f375699d234acecba2ad70c6157d631abcf8e03bb97f6e42'
