b4d2d80d0e5f920899729604ed28900d40644ed9
yoshikihayashi <yoshikixyz57@gmail.com>
2021-01-13 09:42:12 +0000

    非同期通信

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,12 @@
 class SearchController < ApplicationController
-  
+
   def search
     @model = params["search"]["model"]
     @content = params["search"]["content"]
     @how = params["search"]["how"]
     @datas = search_for(@how, @model, @content)
   end
-  
+
   private
   def match(model, content)
     if model == 'user'
